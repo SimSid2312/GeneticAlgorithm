c29c8638fe27d756a201a84d366d59e63f048a43
Urvi <u_l@encs.concordia.ca>
2018-11-18 14:49:18 -0500

    Change stopping criteria

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,8 @@ public class Main {
     static int sameFitnessCount = 0;
     static float lastMaxFitness = -1;
     static float currentMaxFitness = 0;
+    static float lastAverageFitness = -1;
+    static float currentAverageFitness = 0;
 
     public static void main(String[] args) {
 
@@ -25,43 +27,56 @@ public class Main {
         Collections.sort(arrPopulation, individualFitness);
         displayCurrentPopulation();
         
-
-        
         while (sameFitnessCount<5){        	
             lastMaxFitness = arrPopulation.get(0).getFitness();
+            lastAverageFitness = averageFitness();
             Collections.sort(arrPopulation, individualFitness);// this can be commented
             generationCheck();
             displayCurrentPopulation();
+            currentAverageFitness = averageFitness();
             currentMaxFitness = arrPopulation.get(0).getFitness();
-            if (lastMaxFitness == currentMaxFitness){
-            	// System.out.println(sameFitnessCount+" fitness: "+ lastMaxFitness);            	
+            if (lastMaxFitness == currentMaxFitness && lastAverageFitness == currentAverageFitness){
                 sameFitnessCount++;
             }else{
-            	System.out.println("resetting fit count");
+            	System.out.println("Resetting fit count");
                 sameFitnessCount = 0;
             }
-           
         }
         
         System.out.println("\n\n***Final: "+arrPopulation.get(0)+" fitness: "+arrPopulation.get(0).fitness+"***");
+
+    }
+
+    public static float averageFitness(){
+        float fitness = 0;
+
+        for (Individual individual : arrPopulation){
+            fitness = fitness + individual.getFitness();
+        }
+
+        Float averageFitness =  fitness / arrPopulation.size();
+
+        return averageFitness;
     }
 
    
     public static void displayCurrentPopulation(){
-    	
     	 for(Individual indi:arrPopulation){
              System.out.println("Individual: "+indi.getIndividual()+" Fitness: "+indi.getFitness());
          }
     }
+
+    public static boolean areAllElementsOfArrayZero(ArrayList<Integer> arrIndividual){
+        boolean areElementsSame = arrIndividual.stream().distinct().limit(2).count() <= 1 && arrIndividual.get(0) == 0;
+        return areElementsSame;
+    }
     
-    // Comparator Function - Sorting Individual Objects Fitness Property
     public static Comparator<Individual> individualFitness = new Comparator<Individual>() {
 
         public int compare(Individual ind1, Individual ind2) {
 
             float fit1 =  ind1.getFitness();
             float fit2 =  ind2.getFitness();
-            //Descending order sorting--
             if (fit1>fit2)
                 return -1;
             else if (fit2>fit1)
@@ -72,13 +87,11 @@ public class Main {
     };
 
     public static String makeStringFromArray(ArrayList<Integer> arrInvestmentDecision){
-        
     	String lastestIndividualToString = "";
         for(int i=0;i<arrInvestmentDecision.size();i++){
         	lastestIndividualToString+=arrInvestmentDecision.get(i);
         }
         return lastestIndividualToString;
-     
     }
 
     public static ArrayList<Individual> preparePopulation(int populationSize){
@@ -88,6 +101,11 @@ public class Main {
 
         while (arrPopulation.size()<populationSize){
             arrInvestment = prepareInvestmentArray();
+            //We dont want 0000
+            while (areAllElementsOfArrayZero(arrInvestment)){
+                arrInvestment = prepareInvestmentArray();
+            }
+
             if (areAllConstraintSatisfied(arrPopulation,arrInvestment)){
                 arrPopulation.add(new Individual(arrInvestment));
             }
@@ -121,7 +139,6 @@ public class Main {
        			investmentExists=true;
     			break;
     		}
-    			
     	}
         return investmentExists;
     }
@@ -156,6 +173,7 @@ public class Main {
         return false;
     }
 
+
     public static void setFitness(Individual individual) {
         ArrayList<Integer> parent = individual.getIndividual();
         float fitness =  (float) (0.2 * parent.get(0)
@@ -169,32 +187,25 @@ public class Main {
 
     public static void generationCheck(){
 
-        float random = 0 + rn.nextFloat() * (1 - 0);
+        float random = rn.nextFloat() * (1 - 0);
 
         ArrayList<Individual> arrParents = new ArrayList<Individual>();
         ArrayList<Integer> parent1 = new ArrayList<Integer>();
-        ArrayList<Integer> parent2= new ArrayList<Integer>();
+        ArrayList<Integer> parent2 = new ArrayList<Integer>();
         for (int i: arrPopulation.get(0).getIndividual()){
         	parent1.add(i);	
         }
         for (int i:arrPopulation.get(1).getIndividual()){
         	parent2.add(i);
         }
-        
-        
+
         arrParents.add(new Individual(parent1));
         arrParents.add(new Individual(parent2));
         System.out.println("Selected Parents : "+arrParents);
-      //  ArrayList<Individual> arrChild = new ArrayList<>();
-     //  System.out.println("hashcode for arrParents ,parent1 :"+arrParents.get(0).hashCode());
-      // System.out.println("hashcode for arrParents ,parent1:"+arrPopulation.get(0).hashCode());
-        
-        
+
     	if(random<=crossOverProb){
-    	    System.out.println("In crossOver");
             doCrossOver(arrParents);
         }else{
-            System.out.println("In mutation");
             doMutation(arrParents);
         }
 
@@ -204,35 +215,26 @@ public class Main {
         Individual childOne = new Individual(arrParents.get(0).getIndividual());
         Individual childTwo = new Individual(arrParents.get(1).getIndividual());
        
-        // int random = (int)(Math.random() * 4 );//random number of digits
         int random=rn.nextInt(3 - 1 + 1) + 1;
-        System.out.println("random : "+random);
+//        System.out.println("random : "+random);
         for (int i = 0 ; i < 4; i++){
-                
-        	   if(i<(random))
-        	   {             	 
+        	   if(i<(random)) {
               	 System.out.println("Cross over at index: "+ i+" child one :"+childOne.getIndividual().get(i)+" child two: "+childTwo.getIndividual().get(i));
               	 int childOneBit=childOne.getIndividual().get(i);
               	 childOne.getIndividual().set(i,childTwo.getIndividual().get(i)); 
               	 childTwo.getIndividual().set(i,childOneBit);
-                 
         	   }
-            
         }
         
-    
-        System.out.println("CrossOver" +childOne);
-        System.out.println("CrossOver" +childTwo);
-        System.out.println("Current Population :"+arrPopulation);
+        System.out.println("Child One " +childOne);
+        System.out.println("Child Two " +childTwo);
+//        System.out.println("Current Population :"+arrPopulation);
         manipulateChildCreation(childOne,0);
         manipulateChildCreation(childTwo,1);
-        System.out.println("After constraint check :"+arrPopulation);
-        System.out.println("-----");
     }
 
     public static void doMutation(ArrayList<Individual> arrParents){
-
-        for(Individual ind:arrParents){        		
+        for(Individual ind:arrParents){
             for(int i=0;i<ind.getIndividual().size();i++){
                 float random = 0 + rn.nextFloat() * (1 - 0);
                 if(random <=mutationProb ) {
@@ -242,35 +244,24 @@ public class Main {
                     else
                         ind.getIndividual().set(i, 1);
                   }
-               
             }
-    
         }
         System.out.println("Mutation" +arrParents);
         System.out.println("Current Population :"+arrPopulation);
         manipulateChildCreation(arrParents.get(0),0);
         manipulateChildCreation(arrParents.get(1),1);
         System.out.println("After constraint check :"+arrPopulation);
-       System.out.println("-----");
     }
 
     public static void manipulateChildCreation(Individual individual,int index){
     	
-    	
-    	
-        if (areAllConstraintSatisfied(arrPopulation,individual.getIndividual())){
-        	System.out.println("GENERATION CHANGING HERE!!!");
-            arrPopulation.remove(index);            
+        if (areAllConstraintSatisfied(arrPopulation,individual.getIndividual()) && !areAllElementsOfArrayZero(individual.getIndividual())){
+            arrPopulation.remove(index);
             setFitness(individual);//cal and set the fitness of the new kid!!
             arrPopulation.add(individual);
             Collections.sort(arrPopulation, individualFitness);
-        }      
-        else
-        {
-        	System.out.println("CONSTRAINTS OF THE POPULATION NOT MET!!!");
+            System.out.println("child added");
         }
-        	
-        
     }
 
 }
